dc31034963b3e3ab8b6e2034d74f138e78db9443
YEEUN <ykoou@naver.com>
2021-02-08 04:00:28 +0900

    add xml & Fragments

--- app/src/main/java/com/example/handsign/MainActivity.java
+++ app/src/main/java/com/example/handsign/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.handsign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private SignTrans signTrans;
+    private VoiceTrans voiceTrans;
+    private Home home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_main);
+
+        signTrans = new SignTrans();
+        voiceTrans = new VoiceTrans();
+        home = new Home();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomMenu);
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_layout, home).commitAllowingStateLoss();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        FragmentTransaction transaction = fragmentManager.beginTransaction();
+                        switch (item.getItemId()) {
+                            case R.id.signTrans:
+                                //페이지 이동
+                                transaction.replace(R.id.frame_layout, signTrans).commitAllowingStateLoss();
+                                break;
+                            case R.id.return_home:
+                                //페이지 이동
+                                transaction.replace(R.id.frame_layout, home).commitAllowingStateLoss();
+                                break;
+                            case R.id.voiceTrans:
+                                //페이지 이동
+                                transaction.replace(R.id.frame_layout, voiceTrans).commitAllowingStateLoss();
+                                break;
+                        }
+                        return false;
+                    }
+                });
+        };
 }
---
+++ app/src/main/java/com/example/handsign/VoiceTrans.java
@@ -0,0 +1,54 @@
+package com.example.handsign;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VoiceTrans extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.voice_trans, container, false);
+
+        Toolbar toolbar = v.findViewById(R.id.toolbar);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        ActionBar actionBar = activity.getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        return v;
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        MenuInflater menuInflater = getMenuInflater();
+//        menuInflater.inflate(R.menu.toolbar_menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch(item.getItemId()){
+            case R.id.setting:
+                //환경설정 코드
+                break;
+            case android.R.id.home:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
